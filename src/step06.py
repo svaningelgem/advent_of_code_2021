d7f08660c0289b914d888587ecb1746026b633dd
Steven Van Ingelgem <steven@vaningelgem.be>
2021-12-06 19:41:33 +0100

    Finished day 6

--- src/step06.py
+++ src/step06.py
@@ -1,8 +1,45 @@
-
-import sys
-import operator
-import re
 from pathlib import Path
-from typing import Generator, Union, List, Optional
-from collections import namedtuple
+from typing import Union
 
+
+class School:
+    def __init__(self):
+        self._internal: Dict[int, int] = {k: 0 for k in range(9)}
+
+    def add_fish(self, start_number: Union[str, int]) -> None:
+        self._internal[int(start_number)] += 1
+
+    def advance_x_days(self, amount: int = 0) -> None:
+        for _ in range(amount):
+            new_school = {k - 1: v for k, v in self._internal.items()}
+
+            new_school[6] += new_school[-1]
+            new_school[8] = new_school[-1]
+            del new_school[-1]
+
+            self._internal = new_school
+
+    def __len__(self) -> int:
+        return sum(self._internal.values())
+
+    def __str__(self) -> str:
+        return str(self._internal)
+
+
+def create_school(filename) -> School:
+    school = School()
+
+    for nr in filename.read_text().split(','):
+        school.add_fish(nr)
+
+    return school
+
+
+def display_school(school: School):
+    print(school)
+
+
+def simulate_life(filename: Path, amount_of_days: int) -> School:
+    school = create_school(filename)
+    school.advance_x_days(amount_of_days)
+    return school
--- tests/step06_tests.py
+++ tests/step06_tests.py
@@ -1,27 +1,30 @@
 
 from pathlib import Path
 
-from _pytest.fixtures import fixture
+from step06 import simulate_life
 
 TEST_INPUT = Path(__file__).parent / 'step06.txt'
 REAL_INPUT = Path(__file__).parent.parent / 'src/step06.txt'
 
 
-def test_step6(test_data):
-    # assert calculate_position(test_data) == 150
-    pass
+def test_step6():
+    school = simulate_life(TEST_INPUT, 18)
+    assert len(school) == 26
+
+    school = simulate_life(TEST_INPUT, 80)
+    assert len(school) == 5934
 
 
-def test_step6_real_data(real_data):
-    # assert calculate_position(real_data) == 2039256
-    pass
+def test_step6_real_data():
+    school = simulate_life(REAL_INPUT, 80)
+    assert len(school) == 372300
 
 
-def test_step6_part2(test_data):
-    # assert calculate_position_with_aim(test_data) == 900
-    pass
+def test_step6_part2():
+    school = simulate_life(TEST_INPUT, 256)
+    assert len(school) == 26984457539
 
 
-def test_step6_part2_real_data(real_data):
-    # assert calculate_position_with_aim(real_data) == 1856459736
-    pass
+def test_step6_part2_real_data():
+    school = simulate_life(REAL_INPUT, 256)
+    assert len(school) == 1675781200288
