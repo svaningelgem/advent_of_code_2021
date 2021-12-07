7504c120995b134744ad0722c1402f5ce318fcc3
Steven Van Ingelgem <steven@vaningelgem.be>
2021-12-07 07:05:33 +0100

    Finished day 7: brute force calculation. Took 1m 5s :p

--- src/step07.py
+++ src/step07.py
@@ -1,8 +1,42 @@
-
-import sys
-import operator
-import re
+from functools import lru_cache
 from pathlib import Path
-from typing import Generator, Union, List, Optional
-from collections import namedtuple
+from typing import List
 
+
+def _getnumbers(file: Path) -> List[int]:
+    return [
+        int(x)
+        for x in file.read_text().split(',')
+    ]
+
+
+@lru_cache(3000)
+def _get_fuel_cost(from_: int, to_: int, exponential: bool) -> int:
+    min_ = min(from_, to_)
+    max_ = max(from_, to_)
+
+    if exponential:
+        return sum(
+            (i - min_)
+            for i in range(min_, max_+1)
+        )
+    else:
+        return max_ - min_
+
+
+def _what_if_move_all_to(lst: List[int], to_position: int, exponential: bool = False) -> int:
+    fuel_cost = 0
+    for entry in lst:
+        fuel_cost += _get_fuel_cost(entry, to_position, exponential)
+    return fuel_cost
+
+
+def find_least_fuel_position(file: Path, exponential: bool = False) -> int:
+    nrs = _getnumbers(file)
+    min_ = min(nrs)
+    max_ = max(nrs)
+
+    return min(
+        _what_if_move_all_to(nrs, i, exponential)
+        for i in range(min_, max_+1)
+    )
--- tests/step07_tests.py
+++ tests/step07_tests.py
@@ -1,27 +1,24 @@
 
 from pathlib import Path
 
-from _pytest.fixtures import fixture
+from step07 import _get_fuel_cost, find_least_fuel_position
 
 TEST_INPUT = Path(__file__).parent / 'step07.txt'
 REAL_INPUT = Path(__file__).parent.parent / 'src/step07.txt'
 
 
 def test_step7():
-    # assert calculate_position() == 150
-    pass
+    assert find_least_fuel_position(TEST_INPUT) == 37
 
 
 def test_step7_real_data():
-    # assert calculate_position() == 2039256
-    pass
+    assert find_least_fuel_position(REAL_INPUT) == 341534
 
 
 def test_step7_part2():
-    # assert calculate_position_with_aim() == 900
-    pass
+    assert _get_fuel_cost(16, 5, True) == 66
+    assert find_least_fuel_position(TEST_INPUT, True) == 168
 
 
 def test_step7_part2_real_data():
-    # assert calculate_position_with_aim() == 1856459736
-    pass
+    assert find_least_fuel_position(REAL_INPUT, True) == 93397632
