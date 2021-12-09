8a50e7b3573fca3edd8c6bfc194b681f6561f7c6
Steven Van Ingelgem <steven.vaningelgem@kbc.be>
2021-12-09 12:35:34 +0100

    Finished day 9.

--- src/step09.py
+++ src/step09.py
@@ -1,8 +1,90 @@
-
-import sys
-import operator
-import re
 from pathlib import Path
-from typing import Generator, Union, List, Optional
 from collections import namedtuple
+from pathlib import Path
+from typing import Generator, List
 
+LowPoint = namedtuple('LowPoint', 'x y value')
+
+
+def _is_low_point_protected(matrix: List[List[int]], x: int, y: int, x2: int, y2: int) -> bool:
+    if x2 < 0 or y2 < 0:
+        return True
+
+    try:
+        return matrix[y][x] < matrix[y2][x2]
+    except (KeyError, IndexError):
+        return True
+
+
+def _is_a_9(matrix: List[List[int]], x: int, y: int) -> bool:
+    try:
+        return matrix[y][x] == 9
+    except (KeyError, IndexError):
+        return True  # Borders also considered to be a 9
+
+
+def _is_low_point(matrix: List[List[int]], x: int, y: int) -> bool:
+    return (
+        _is_low_point_protected(matrix, x, y, x, y - 1)
+        and _is_low_point_protected(matrix, x, y, x, y + 1)
+        and _is_low_point_protected(matrix, x, y, x - 1, y)
+        and _is_low_point_protected(matrix, x, y, x + 1, y)
+    )
+
+
+def _get_matrix(file: Path) -> List[List[int]]:
+    return [
+        [int(c) for c in line]
+        for line in file.read_text().splitlines()
+    ]
+
+
+def _find_all_lowest_points(matrix: List[List[int]]) -> Generator[LowPoint, None, None]:
+    for x in range(len(matrix[0])):
+        for y in range(len(matrix)):
+            if _is_low_point(matrix, x, y):
+                yield LowPoint(x, y, matrix[y][x])
+
+
+def find_low_points(file: Path) -> int:
+    matrix = _get_matrix(file)
+
+    low_points = list(_find_all_lowest_points(matrix))
+
+    return sum(x.value for x in low_points) + len(low_points)
+
+
+def _find_basin_for(matrix: List[List[int]], x: int, y: int, final: List[LowPoint]) -> None:
+    # A basin flows from the x,y position of the lowest point, and is bordered by 9's (not inclusive)
+    if x < 0 or y < 0:
+        return
+    if _is_a_9(matrix, x, y):
+        return  # Final step
+    if any(x == pt.x and y == pt.y for pt in final):
+        return  # Already been here.
+
+    # Add field
+    final.append(LowPoint(x, y, matrix[y][x]))
+
+    # And branch outwards
+    _find_basin_for(matrix, x - 1, y, final)
+    _find_basin_for(matrix, x + 1, y, final)
+    _find_basin_for(matrix, x, y - 1, final)
+    _find_basin_for(matrix, x, y + 1, final)
+
+
+def find_basins(file: Path) -> int:
+    matrix = _get_matrix(file)
+
+    low_points = list(_find_all_lowest_points(matrix))
+
+    # For each low point: find the basin it is in.
+    all_basins = []
+    for lp in low_points:
+        final = []
+        _find_basin_for(matrix, lp.x, lp.y, final)
+        all_basins.append(final)
+
+    all_basins = sorted(all_basins, key=len)
+
+    return len(all_basins[-1]) * len(all_basins[-2]) * len(all_basins[-3])
--- tests/step09_tests.py
+++ tests/step09_tests.py
@@ -1,27 +1,23 @@
 
 from pathlib import Path
 
-from _pytest.fixtures import fixture
+from step09 import find_low_points, find_basins
 
 TEST_INPUT = Path(__file__).parent / 'step09.txt'
 REAL_INPUT = Path(__file__).parent.parent / 'src/step09.txt'
 
 
 def test_step9():
-    # assert calculate_position() == 150
-    pass
+    assert find_low_points(TEST_INPUT) == 15
 
 
 def test_step9_real_data():
-    # assert calculate_position() == 2039256
-    pass
+    assert find_low_points(REAL_INPUT) == 486
 
 
 def test_step9_part2():
-    # assert calculate_position_with_aim() == 900
-    pass
+    assert find_basins(TEST_INPUT) == 1134
 
 
 def test_step9_part2_real_data():
-    # assert calculate_position_with_aim() == 1856459736
-    pass
+    assert find_basins(REAL_INPUT) == 1059300
