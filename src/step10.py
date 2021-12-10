90db76f774c29fe4c24f376e0e1356bb547c08ec
Steven Van Ingelgem <steven@vaningelgem.be>
2021-12-10 07:14:34 +0100

    Finished day 10.

--- src/step10.py
+++ src/step10.py
@@ -1,8 +1,80 @@
-
-import sys
-import operator
-import re
 from pathlib import Path
-from typing import Generator, Union, List, Optional
-from collections import namedtuple
 
+
+class CorruptedLine(Exception):
+    def __init__(self, char):
+        self.char = char
+
+
+class IncompleteLine(Exception):
+    def __init__(self, expected):
+        self.expected = expected
+
+
+def process_line(line) -> None:
+    seen = {
+        '{': '}',
+        '[': ']',
+        '(': ')',
+        '<': '>',
+    }
+
+    expected = []
+    for idx, c in enumerate(line):
+        if not expected or c in seen:
+            expected.append(seen[c])
+            continue
+
+        if c == expected[-1]:
+            expected.pop()
+            continue
+
+        raise CorruptedLine(c)
+
+    if expected:
+        raise IncompleteLine(expected)
+
+
+def find_score_part1(file: Path) -> int:
+    score = 0
+    score_table = {
+        ')': 3,
+        ']': 57,
+        '}': 1197,
+        '>': 25137,
+    }
+
+    for line in file.read_text().splitlines():
+        try:
+            process_line(line)
+        except IncompleteLine:
+            pass
+        except CorruptedLine as ex:
+            score += score_table[ex.char]
+
+    return score
+
+
+def autocomplete_score(file: Path) -> int:
+    score_table = {
+        ')': 1,
+        ']': 2,
+        '}': 3,
+        '>': 4,
+    }
+
+    scores = []
+    for line in file.read_text().splitlines():
+        try:
+            process_line(line)
+        except CorruptedLine:
+            pass
+        except IncompleteLine as ex:
+            tmp = 0
+            for c in reversed(ex.expected):
+                tmp *= 5
+                tmp += score_table[c]
+            scores.append(tmp)
+
+    scores = sorted(scores)
+    return scores[len(scores) // 2]
--- tests/step10_tests.py
+++ tests/step10_tests.py
@@ -1,27 +1,22 @@
-
 from pathlib import Path
 
-from _pytest.fixtures import fixture
+from step10 import autocomplete_score, find_score_part1
 
 TEST_INPUT = Path(__file__).parent / 'step10.txt'
 REAL_INPUT = Path(__file__).parent.parent / 'src/step10.txt'
 
 
 def test_step10():
-    # assert calculate_position() == 150
-    pass
+    assert find_score_part1(TEST_INPUT) == 26397
 
 
 def test_step10_real_data():
-    # assert calculate_position() == 2039256
-    pass
+    assert find_score_part1(REAL_INPUT) == 318081
 
 
 def test_step10_part2():
-    # assert calculate_position_with_aim() == 900
-    pass
+    assert autocomplete_score(TEST_INPUT) == 288957
 
 
 def test_step10_part2_real_data():
-    # assert calculate_position_with_aim() == 1856459736
-    pass
+    assert autocomplete_score(REAL_INPUT) == 4361305341
