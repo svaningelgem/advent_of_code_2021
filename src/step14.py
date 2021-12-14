ce69823cc9b1d7999c4aa83e8aee507e9957e6df
Steven Van Ingelgem <steven.vaningelgem@kbc.be>
2021-12-14 11:29:48 +0100

    Finished day 14

--- src/step14.py
+++ src/step14.py
@@ -1,6 +1,67 @@
-import sys
-import operator
-import re
+from collections import defaultdict
 from pathlib import Path
-from typing import Generator, Union, List, Optional
-from collections import namedtuple
+from typing import Dict, Tuple
+
+
+def _read_file(file: Path) -> Tuple[str, Dict[str, Tuple[str, str]]]:
+    first_line = None
+    replacements = {}
+
+    for line in file.read_text().splitlines():
+        line = line.strip()
+        if not line: continue
+
+        if first_line is None:
+            first_line = line
+            continue
+
+        from_, insertion = map(str.strip, line.split('->'))
+        assert from_ not in replacements
+        replacements[from_] = (from_[0] + insertion, insertion + from_[1])
+
+    return first_line, replacements
+
+
+def _split_polymer_in_parts(template) -> Dict[str, int]:
+    tmp = defaultdict(int)
+    for i in range(len(template)):
+        part = template[i:i+2]
+        if len(part) != 2: continue
+
+        tmp[part] += 1
+    return tmp
+
+
+def _perform_insertions(file: Path, times: int = 10) -> Dict[str, int]:
+    template, replacements = _read_file(file)
+
+    current_state = _split_polymer_in_parts(template)
+
+    for _ in range(times):
+        previous_parts = [(k, v) for k, v in current_state.items() if v > 0]
+        for part, amount in previous_parts:
+            if part not in replacements: continue
+
+            current_state[part] -= amount
+            for repl in replacements[part]:
+                current_state[repl] += amount
+
+    return current_state
+
+
+def _custom_count(template: Dict[str, int]) -> Dict[str, int]:
+    counter = defaultdict(int)
+    for idx, (k, v) in enumerate(template.items()):
+        if idx == 0:  # Only count the first entry
+            counter[k[0]] += v
+
+        counter[k[1]] += v
+    return counter
+
+
+def count_least_most_after_insertions(file: Path, times: int) -> int:
+    calculated_template = _perform_insertions(file, times)
+
+    counter = _custom_count(calculated_template)
+
+    return max(counter.values()) - min(counter.values())
--- tests/step14_tests.py
+++ tests/step14_tests.py
@@ -1,24 +1,28 @@
 from pathlib import Path
 
+from step14 import count_least_most_after_insertions
+
 TEST_INPUT = Path(__file__).parent / 'step14.txt'
 REAL_INPUT = Path(__file__).parent.parent / 'src/step14.txt'
 
 
 def test_step14():
-    # assert calculate_position() == 150
-    pass
+    assert count_least_most_after_insertions(TEST_INPUT, 0) == 1  # 'NNCB'
+    assert count_least_most_after_insertions(TEST_INPUT, 1) == 1  # 'NCNBCHB'
+    assert count_least_most_after_insertions(TEST_INPUT, 2) == 5  # 'NBCCNBBBCBHCB'
+    assert count_least_most_after_insertions(TEST_INPUT, 3) == 7  # 'NBBBCNCCNBBNBNBBCHBHHBCHB'
+    assert count_least_most_after_insertions(TEST_INPUT, 4) == 18  # 'NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB'
+
+    assert count_least_most_after_insertions(TEST_INPUT, 10) == 1588
 
 
 def test_step14_real_data():
-    # assert calculate_position() == 2039256
-    pass
+    assert count_least_most_after_insertions(REAL_INPUT, 10) == 2223
 
 
 def test_step14_part2():
-    # assert calculate_position_with_aim() == 900
-    pass
+    assert count_least_most_after_insertions(TEST_INPUT, 40) == 2188189693529
 
 
 def test_step14_part2_real_data():
-    # assert calculate_position_with_aim() == 1856459736
-    pass
+    assert count_least_most_after_insertions(REAL_INPUT, 40) == 2566282754493
