80351508d6d6953392bc57af20e1fac05ab3ec2a
Steven Van Ingelgem <steven.vaningelgem@kbc.be>
2021-12-16 13:57:18 +0100

    Finished day 16

--- src/step16.py
+++ src/step16.py
@@ -1,6 +1,99 @@
-import sys
+import io
 import operator
-import re
-from pathlib import Path
-from typing import Generator, Union, List, Optional
-from collections import namedtuple
+from functools import reduce
+from typing import Union
+
+
+def decode_hex(txt: str) -> str:
+    s = bin(int(txt.strip(), 16))[2:]  # strip '0b' in front of it.
+    if len(s) % 8 != 0:  # Not exactly 8 positions
+        s = s.zfill(8 * ((len(s) // 8) + 1))
+    return s
+
+
+
+
+class Packet:
+    def __init__(self, packet: Union[io.StringIO, str]):
+        if isinstance(packet, str):
+            packet = io.StringIO(decode_hex(packet))
+
+        self.version = int(packet.read(3), 2)
+        self.packet_id = int(packet.read(3), 2)
+
+        self.is_literal = self.packet_id == 4
+
+        self._value = None
+        self.sub_packets = []
+        self.decode_contents(packet)
+
+    def decode_contents(self, packet: io.StringIO):
+        if self.is_literal:
+            self.decode_literal(packet)
+        else:
+            self.decode_operator(packet)
+
+    def decode_literal(self, packet: io.StringIO):
+        should_stop = False
+        literal = ''
+        while not should_stop:
+            current_value = packet.read(5)
+            should_stop = current_value[0] == '0'
+            literal += current_value[1:]
+        self._value = int(literal, 2)
+
+    def decode_operator(self, packet):
+        length_type_id = packet.read(1)
+        if length_type_id == '0':
+            size_to_read = int(packet.read(15), 2)
+            bits_to_interpret = packet.read(size_to_read)
+            new_stream = io.StringIO(bits_to_interpret)
+            while new_stream.tell() < size_to_read:
+                self.sub_packets.append(Packet(new_stream))
+        else:
+            packets_to_read = int(packet.read(11), 2)
+            for _ in range(packets_to_read):
+                self.sub_packets.append(Packet(packet))
+
+    @property
+    def value(self) -> int:
+        sub_packets_values = [x.value for x in self.sub_packets]
+
+        if self.packet_id == 0:  # sum
+            return sum(sub_packets_values)
+
+        if self.packet_id == 1:  # product
+            return reduce(operator.mul, sub_packets_values, 1)
+
+        if self.packet_id == 2:  # min
+            return min(sub_packets_values)
+
+        if self.packet_id == 3:  # max
+            return max(sub_packets_values)
+
+        if self.packet_id == 4:  # -- literal
+            return self._value
+
+        if self.packet_id == 5:  # greater than
+            assert len(sub_packets_values) == 2
+            return 1 if sub_packets_values[0] > sub_packets_values[1] else 0
+
+        if self.packet_id == 6:  # less than
+            assert len(self.sub_packets) == 2
+            return 1 if sub_packets_values[0] < sub_packets_values[1] else 0
+
+        if self.packet_id == 7:  # equal
+            assert len(self.sub_packets) == 2
+            return 1 if sub_packets_values[0] == sub_packets_values[1] else 0
+
+
+def get_version_sum(packet):
+    total = 0
+
+    packets_to_count = [Packet(packet)]
+    while packets_to_count:
+        current = packets_to_count.pop()
+        total += current.version
+        packets_to_count.extend(current.sub_packets)
+
+    return total
--- tests/step16_tests.py
+++ tests/step16_tests.py
@@ -1,24 +1,61 @@
 from pathlib import Path
 
+from step16 import decode_hex, Packet, get_version_sum
+
 TEST_INPUT = Path(__file__).parent / 'step16.txt'
 REAL_INPUT = Path(__file__).parent.parent / 'src/step16.txt'
 
 
 def test_step16():
-    # assert calculate_position() == 150
-    pass
+    assert decode_hex('D2FE28') == '110100101111111000101000'
+    assert decode_hex('38006F45291200') == '00111000000000000110111101000101001010010001001000000000'
+    assert decode_hex('EE00D40C823060') == '11101110000000001101010000001100100000100011000001100000'
+
+    sut = Packet('D2FE28')
+    assert sut.version == 6
+    assert sut.packet_id == 4
+    assert sut.is_literal
+    assert sut._value == 2021
+
+    sut = Packet('38006F45291200')
+    assert sut.version == 1
+    assert sut.packet_id == 6
+    assert not sut.is_literal
+    assert sut._value is None
+    assert len(sut.sub_packets) == 2
+    assert sut.sub_packets[0]._value == 10
+    assert sut.sub_packets[1]._value == 20
+
+    sut = Packet('EE00D40C823060')
+    assert sut.version == 7
+    assert sut.packet_id == 3
+    assert not sut.is_literal
+    assert sut._value is None
+    assert len(sut.sub_packets) == 3
+    assert sut.sub_packets[0]._value == 1
+    assert sut.sub_packets[1]._value == 2
+    assert sut.sub_packets[2]._value == 3
+
+    assert get_version_sum('8A004A801A8002F478') == 16
+    assert get_version_sum('620080001611562C8802118E34') == 12
+    assert get_version_sum('C0015000016115A2E0802F182340') == 23
+    assert get_version_sum('A0016C880162017C3686B18A3D4780') == 31
 
 
 def test_step16_real_data():
-    # assert calculate_position() == 2039256
-    pass
+    assert get_version_sum(REAL_INPUT.read_text()) == 879
 
 
 def test_step16_part2():
-    # assert calculate_position_with_aim() == 900
-    pass
+    assert Packet('C200B40A82').value == 3
+    assert Packet('04005AC33890').value == 54
+    assert Packet('880086C3E88112').value == 7
+    assert Packet('CE00C43D881120').value == 9
+    assert Packet('D8005AC2A8F0').value == 1
+    assert Packet('F600BC2D8F').value == 0
+    assert Packet('9C005AC2F8F0').value == 0
+    assert Packet('9C0141080250320F1802104A08').value == 1
 
 
 def test_step16_part2_real_data():
-    # assert calculate_position_with_aim() == 1856459736
-    pass
+    assert Packet(REAL_INPUT.read_text()).value == 539051801941
